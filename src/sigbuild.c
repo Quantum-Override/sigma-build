ccbb789477a5000d93097e5c89c38d370e9e2914
badkraft <thebadkraft@gmail.com>
2025-05-30 08:20:13 -0500

    bootstrap build 02
    2nd bootstrap build after implementing VarTable and inline var replacement

--- src/sigbuild.c
+++ src/sigbuild.c
@@ -9,13 +9,13 @@
  */
 
 #include "core.h"
+#include "core/builder.h"
 #include "core/cli_parser.h"
 #include "core/loader.h"
-#include "core/builder.h"
 #include <errno.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define SIGMABUILD_VERSION "0.00.01"
@@ -59,19 +59,16 @@ static const char *DEBUG_LEVELS[] = {
 
 // General Helper Functions
 // Function to get the current timestamp in a specified format
-void get_timestamp(char *buffer, const char *format)
-{
+void get_timestamp(char *buffer, const char *format) {
    time_t now = time(NULL);
    strftime(buffer, 32, format, localtime(&now));
 }
 
 // CLI App Functions
 // This function initializes the CLI state with the provided arguments
-void cli_init(int argc, char **args)
-{
+void cli_init(int argc, char **args) {
    // Register the cleanup function to be called on exit
-   if (atexit(cli_cleanup) != 0)
-   {
+   if (atexit(cli_cleanup) != 0) {
       perror("Failed to register cleanup function");
       exit(EXIT_FAILURE);
    }
@@ -81,8 +78,7 @@ void cli_init(int argc, char **args)
    cli_init_state(argc, args);
    // Parse command line arguments
    CLI.parse_args(argc, args, &cli_state->options, &cli_state->error);
-   if (cli_state->error != CLI_SUCCESS)
-   {
+   if (cli_state->error != CLI_SUCCESS) {
       // Handle parsing errors
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Error parsing command line arguments: %s\n",
               cli_get_err_msg(cli_state->error));
@@ -98,12 +94,10 @@ void cli_init(int argc, char **args)
    context->data = NULL;                                   // No additional data by default
 }
 // This function initializes the build context with default values
-void cli_init_context(void)
-{
+void cli_init_context(void) {
    // Allocate memory for the build context
    context = (BuildContext)malloc(sizeof(struct build_context_s));
-   if (!context)
-   {
+   if (!context) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Failed to allocate memory for build context.\n");
       exit(EXIT_FAILURE);
    }
@@ -118,19 +112,16 @@ void cli_init_context(void)
    context->data = NULL;                    // No additional data by default
 }
 // This function initializes the CLI state with default values
-void cli_init_state(int argc, char **args)
-{
+void cli_init_state(int argc, char **args) {
    cli_state = (CLIState)malloc(sizeof(struct cli_state_s));
-   if (!cli_state)
-   {
+   if (!cli_state) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Failed to allocate memory for CLI state.\n");
       exit(EXIT_FAILURE);
    }
    cli_state->argv = args;
    cli_state->argc = argc;
    cli_state->options = (CLIOptions)malloc(sizeof(struct cli_options_s));
-   if (!cli_state->options)
-   {
+   if (!cli_state->options) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Failed to allocate memory for CLI options.\n");
       free(cli_state);
       exit(EXIT_FAILURE);
@@ -145,26 +136,22 @@ void cli_init_state(int argc, char **args)
    cli_state->error = CLI_SUCCESS;             // Initialize error code to success
 }
 // Load the configuration file specified in the command line options
-int cli_load_config(void)
-{
+int cli_load_config(void) {
    writelnf("Loading configuration file: %s", cli_state->options->config_file ? cli_state->options->config_file : "None");
-   if (cli_state->options->config_file == NULL)
-   {
+   if (cli_state->options->config_file == NULL) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Configuration file is missing.\n");
       return LOADER_ERR_PARSE_FAILED; // Return error if config file is missing
    }
    // Here you would typically load the configuration file
    // For now, we just simulate a successful load
-   if (strcmp(cli_state->options->config_file, "invalid.json") == 0)
-   {
+   if (strcmp(cli_state->options->config_file, "invalid.json") == 0) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Invalid configuration file specified: %s\n", cli_state->options->config_file);
       return LOADER_ERR_FILE_NOT_FOUND; // Return error if config file is invalid
    }
 
    // Load configuration
    BuildConfig config = Loader.load_config(cli_state->options->config_file);
-   if (!config)
-   {
+   if (!config) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Failed to load configuration from file: %s\n", cli_state->options->config_file);
       return LOADER_ERR_PARSE_FAILED; // Return error if config file could not be parsed
    }
@@ -175,30 +162,23 @@ int cli_load_config(void)
    return CLI_SUCCESS; // Return success if config file is loaded successfully
 }
 // Run the build application
-void cli_run(void)
-{
-   if (cli_state->options->show_help)
-   {
+void cli_run(void) {
+   if (cli_state->options->show_help) {
       cli_display_help();
       return;
-   }
-   else if (cli_state->options->show_about)
-   {
+   } else if (cli_state->options->show_about) {
       cli_display_about();
       return;
    }
    // Load the configuration file if specified
-   if (cli_load_config() != CLI_SUCCESS)
-   {
+   if (cli_load_config() != CLI_SUCCESS) {
       fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "Failed to load configuration file: %s\n",
               cli_get_err_msg(LOADER_ERR_LOAD_CONFIG));
       exit(EXIT_FAILURE);
    }
    BuildConfig config = (BuildConfig)context->data; // Get the loaded configuration from the context
-   for (BuildTarget *target = config->targets; target && *target; target++)
-   {
-      if (Builder.build(*target) != 0)
-      {
+   for (BuildTarget *target = config->targets; target && *target; target++) {
+      if (Builder.build(*target) != 0) {
          fdebugf(stderr, LOG_NORMAL, DBG_ERROR, "%s: %s\n",
                  cli_get_err_msg(BUILD_ERR_BUILD_TARGET), (*target)->name);
          exit(EXIT_FAILURE); // Exit if any build target fails
@@ -206,39 +186,43 @@ void cli_run(void)
    }
 }
 // Cleanup function to free resources allocated during the CLI initialization
-static void cli_cleanup(void)
-{
+static void cli_cleanup(void) {
    static int is_disposed = 0; // Flag to ensure cleanup is only done once
-   if (is_disposed)
-   {
+   if (is_disposed) {
       return;
    }
 
-   if (cli_state)
-   {
-      if (cli_state->options)
-      {
+   if (context) {
+      // Free any resources allocated in the context
+      if (context->data) {
+         resources_dispose_config((BuildConfig)context->data);
+         context->data = NULL;
+      }
+      free(context->current_target);
+      free(context->current_configuration);
+      free(context);
+      context = NULL; // Set to NULL after freeing
+   }
+
+   if (cli_state) {
+      if (cli_state->options) {
+         free(cli_state->options->config_file);
          free(cli_state->options);
          cli_state->options = NULL; // Set to NULL after freeing
       }
       free(cli_state);
       cli_state = NULL; // Set to NULL after freeing
    }
-   if (context)
-   {
-      // Free any resources allocated in the context
-      free(context);
-      context = NULL; // Set to NULL after freeing
-   }
+
+   Loader.cleanup();
+
    is_disposed = 1; // Set the flag to indicate cleanup has been done
    fdebugf(stdout, LOG_NORMAL, DBG_INFO, "Cleanup completed for Sigma.Build.\n");
 }
 
 // Get the error message for a given CLIErrorCode
-const char *cli_get_err_msg(CLIErrorCode code)
-{
-   switch (code)
-   {
+const char *cli_get_err_msg(CLIErrorCode code) {
+   switch (code) {
    case CLI_SUCCESS:
       return "No error";
    case CLI_ERR_PARSE_INVALID_ARG:
@@ -278,8 +262,7 @@ const char *cli_get_err_msg(CLIErrorCode code)
    }
 }
 // Display help information for the CLI application
-void cli_display_help(void)
-{
+void cli_display_help(void) {
    char *app = strrchr(cli_state->argv[0], '/');
    app = app ? app + 1 : cli_state->argv[0]; // Get the application name from the path
    // Build options string
@@ -295,11 +278,9 @@ void cli_display_help(void)
    writelnf("  %-6s%-19s Set the log level", OPT_LOG_LEVEL, "(0-2)");
 }
 // Display application and optional component versions
-void cli_display_about(void)
-{
+void cli_display_about(void) {
    writelnf("%s v.%s", SIGMABUILD_NAME, SIGMABUILD_VERSION);
-   if (cli_state->options->is_verbose)
-   {
+   if (cli_state->options->is_verbose) {
       writelnf("Sigma.Build is a build system for C projects.");
       writelnf("%-15s                    %s", "David Boarman", "05-25-2025");
       writelnf("Components:");
@@ -312,14 +293,12 @@ void cli_display_about(void)
 
 /* Logger functions to format output */
 // base logging function
-static void log_message(FILE *stream, const char *fmt, va_list args)
-{
+static void log_message(FILE *stream, const char *fmt, va_list args) {
    vfprintf(stream, fmt, args);
    fflush(stream);
 }
 // This function is used to write formatted messages to the log stream
-void writef(const char *fmt, ...)
-{
+void writef(const char *fmt, ...) {
    va_list args;
    va_start(args, fmt);
 
@@ -328,8 +307,7 @@ void writef(const char *fmt, ...)
    va_end(args);
 }
 // This function is used to write formatted messages with a newline to the log stream
-void writelnf(const char *fmt, ...)
-{
+void writelnf(const char *fmt, ...) {
    va_list args;
    va_start(args, fmt);
 
@@ -341,8 +319,7 @@ void writelnf(const char *fmt, ...)
    va_end(args);
 }
 // This function is used to write formatted messages to the given stream
-void fwritef(FILE *stream, const char *fmt, ...)
-{
+void fwritef(FILE *stream, const char *fmt, ...) {
    va_list args;
    va_start(args, fmt);
 
@@ -352,8 +329,7 @@ void fwritef(FILE *stream, const char *fmt, ...)
    va_end(args);
 }
 // This function is used to write formatted messages with a newline to the given stream
-void fwritelnf(FILE *stream, const char *fmt, ...)
-{
+void fwritelnf(FILE *stream, const char *fmt, ...) {
    va_list args;
    va_start(args, fmt);
 
@@ -365,11 +341,9 @@ void fwritelnf(FILE *stream, const char *fmt, ...)
    va_end(args);
 }
 // Debug logging function
-void fdebugf(FILE *stream, LogLevel log_level, DebugLevel debug_level, const char *fmt, ...)
-{
+void fdebugf(FILE *stream, LogLevel log_level, DebugLevel debug_level, const char *fmt, ...) {
    // Suppress non-error messages if context or caller specify LOG_NONE.
-   if (log_level == LOG_NONE || ((context ? context->log_level == LOG_NONE : 0) && debug_level < DBG_ERROR))
-   {
+   if (log_level == LOG_NONE || ((context ? context->log_level == LOG_NONE : 0) && debug_level < DBG_ERROR)) {
       return; // No output for non-error messages when log level is LOG_NONE
    }
 
@@ -378,8 +352,7 @@ void fdebugf(FILE *stream, LogLevel log_level, DebugLevel debug_level, const cha
 
    // build debug label
    char dbg_label[16] = "[UNKNOWN]";
-   if (debug_level >= 0 && debug_level < sizeof(DEBUG_LEVELS) / sizeof(DEBUG_LEVELS[0]) - 1)
-   {
+   if (debug_level >= 0 && debug_level < sizeof(DEBUG_LEVELS) / sizeof(DEBUG_LEVELS[0]) - 1) {
       snprintf(dbg_label, sizeof(dbg_label), "[%s]", DEBUG_LEVELS[debug_level]);
    }
    //  build the message
@@ -394,8 +367,7 @@ void fdebugf(FILE *stream, LogLevel log_level, DebugLevel debug_level, const cha
    if ((context ? context->log_level == LOG_VERBOSE : 1) ||
        (log_level != LOG_NONE && log_level >= (context ? context->log_level : LOG_NORMAL) &&
         debug_level >= (context ? context->debug_level : DBG_DEBUG)) ||
-       debug_level >= DBG_ERROR)
-   {
+       debug_level >= DBG_ERROR) {
       log_message(stream, msg, args);
    }
 
@@ -404,8 +376,7 @@ void fdebugf(FILE *stream, LogLevel log_level, DebugLevel debug_level, const cha
 
 /* Resource functions */
 // This function disposes of the configuration resources
-void resources_dispose_config(BuildConfig config)
-{
+void resources_dispose_config(BuildConfig config) {
    if (!config)
       return; // Nothing to dispose of
 
@@ -416,16 +387,14 @@ void resources_dispose_config(BuildConfig config)
    free(config->variables);
 
    // Dispose of each target
-   for (BuildTarget *target = config->targets; target && *target; target++)
-   {
+   for (BuildTarget *target = config->targets; target && *target; target++) {
       Resources.dispose_target(*target);
    }
    free(config->targets);
    free(config); // Finally, free the config itself
 }
 // This function disposes of the target resources
-void resources_dispose_target(BuildTarget target)
-{
+void resources_dispose_target(BuildTarget target) {
    if (!target)
       return; // Nothing to dispose of
 
